1b73e7426dd0667a423e8341af31145a30be148c
박찬희 <103471192+PPPCCCCc@users.noreply.github.com>
2022-05-13 02:14:08 +0900

    Delete hr.sql

--- EX/hr.sql
+++
@@ -1,6 +0,0 @@
-select * from employees;
-select * from departments;
-select employee_id, FIRST_name, last_name 
-from employees;
-select first_name, phone_number, hire_date, salary
-from employees;
